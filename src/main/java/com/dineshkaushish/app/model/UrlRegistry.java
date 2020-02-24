a40b61573e56278adc6e6311ac1ec71d42b47d0a
Dinesh Kaushish <dinesh.k.kaushish@gmail.com>
2020-02-24 16:01:17 +0000

    Added Documentation for URL registry class.

--- src/main/java/com/dineshkaushish/app/model/UrlRegistry.java
+++ src/main/java/com/dineshkaushish/app/model/UrlRegistry.java
@@ -5,6 +5,13 @@ import java.util.Map;
 import java.util.Optional;
 
 /**
+ * This class represents a registry for URL links for web scraping according to
+ * the requirements of user input. In this example, a class is used as a cache for URL links
+ * but in a more practical example perhaps a database call or storage in a properties file is more appropriate.
+ *
+ * @Author Dinesh Kaushish
+ * @Version 1.0
+ * Date: 19/02/19
  * Created by dineshkaushish on 23/02/2020.
  */
 public class UrlRegistry {
@@ -13,10 +20,17 @@ public class UrlRegistry {
 
     private Map<String, String> urlLookupMap = new HashMap<>();
 
+    /**
+     * A constructor which initialises the hashmap for url lookup
+     */
     private UrlRegistry(){
         urlLookupMap.put("Cherries and Berries", "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html?language=Java");
     }
 
+    /**
+     * Provides an instance of Url Registry
+     * @return UrlRegistry Singleton Class
+     */
     public static UrlRegistry getInstance(){
         if(instance == null){
             instance = new UrlRegistry();
@@ -25,6 +39,11 @@ public class UrlRegistry {
         return instance;
     }
 
+    /**
+     * Fetches corresponding URL link according to Hashmap key.
+     * @param productCategory
+     * @return Optional<String> of URL link
+     */
     public Optional<String> fetchUrlLink(String productCategory){
         return Optional.of(urlLookupMap.get(productCategory));
     }
